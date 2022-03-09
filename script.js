d5acb71a665f984e567270840c3cc355fe7705fb
InaiaKenia <inaiakenia15@gmail.com>
2022-03-08 23:01:29 -0700

    Included window.confirm

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var 
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+sign = window.prompt('How many characters would you likeyour password contain?');
+
+if(window.confirm("Click OK to confirm including Special characters.")) { }
+
+if(window.confirm("Click OK to confirm including Numeric characters.")) { }
+
+if(window.confirm("Click OK to confirm including UPPERcase characters.")) { }
+
+if(window.confirm("Click OK to confirm including LOWERcase characters.")) { }
+
+
+// generate passcode
+if (window.confirm("Do you really want to leave?")) {
+  window.open("exit.html", "Thanks for Visiting!");
+}
+
 generateBtn.addEventListener("click", writePassword);
