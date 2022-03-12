7aa4298cbb5efda3254122f55fa10f354a1c74d7
InaiaKenia <inaiakenia15@gmail.com>
2022-03-12 14:19:00 -0700

    PROMPTS + CONSOLE.LOG for user answers.

--- script.js
+++ script.js
@@ -5,11 +5,24 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;}
+  passwordText.value = password;
+}
+for(var i =0; i <=passwordText; i++){
+  password = passwordText.value + lengthCha + randomFunc + value.charAt(Math.floor(Math.random()*Math.floor(value.length-1)));
+}
+document.getElementById('#generate').passwordText.value = password;
 
- sign = window.prompt("How many characters would  you likey our password contain?");
 
-if(window.confirm("Click OK to confirm including Special characters.")) { }
+// The best to be as length ? 
+document.getElementById("#generate").innerHTML = "Min 08 ; Max 128 ";
+
+  let lengthCha = prompt('How many characters would  you likey our password contain?');
+  for (let i = 0; i < passLength; i+=secInt) {
+  let length = Number(8 < 128);}
+
+
+ 
+if(window.confirm("Click OK to confirm including Special characters.")) {}
 
 if(window.confirm("Click OK to confirm including Numeric characters.")) { }
 
@@ -17,16 +30,25 @@ if(window.confirm("Click OK to confirm including UPPERcase characters.")) { }
 
 if(window.confirm("Click OK to confirm including LOWERcase characters.")) { }
 
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol,
-}
+
+
+var randomFunc = 'lower: getRandomLower'; 'upper: getRandomUpper'; 'number: getRandomNumber'; 'symbol: getRandomSymbol';
 
 // https://net-comber.com/charset.html
 
-  function getRandomLower() {
+
+function passwordLength(){
+  var password = document.getElementById("password");
+  if(textbox.value.length <= 8 && textbox.value.length >= 128){
+      alert("success");
+  }
+  else{
+      alert("make sure the input is between 8-128 characters long")
+  }
+
+
+
+ function getRandomLower() {
   return String.fromCharCode((Math.floor(Math.random()* 26)+ 97));
 }
 
@@ -44,18 +66,9 @@ console.log(getRandomNumber());
 
 
 function getRandomSymbol() {
-  const symbols = '!@#$%^&*(){}=<>/,.'
-  return symbols [Math.floor(Math.random()*symbols.length)];
+const symbols = '!@#$%^&*(){}=<>/,.'
+return symbols [Math.floor(Math.random()*symbols.length)];
 }
   console.log(getRandomSymbol());
 
 
-
-
-
-
-// Set up a function ( with IF & ELSE statment ) OR Arrays first? 
-
-// How generate randown arrays for characters  ?
-
-// generate passcode.//
